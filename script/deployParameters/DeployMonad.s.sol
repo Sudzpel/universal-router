ce4cd1578ffd6d665ed1958544e150875c7a5004
Sudzpel <114531512+Sudzpel@users.noreply.github.com>
2025-11-12 20:25:59 +0300

    update: pool-pair init hash and contracts for production

--- script/deployParameters/DeployMonad.s.sol
+++ script/deployParameters/DeployMonad.s.sol
@@ -11,10 +11,10 @@ contract DeployMonad is DeployUniversalRouter {
             weth9: 0x3bd359C1119dA7Da1D913D1C4D2B7c461115433A,
             v2Factory: 0xCe104732685B9D7b2F07A09d828F6b19786cdA32,
             v3Factory: 0x30Db57A29ACf3641dfc3885AF2e5f1F5A408D9CB,
-            pairInitCodeHash: 0x0757d600d332ab414bff776d7ba09eb55c029272b57d995e41f571edfb1f6fbe,
-            poolInitCodeHash: 0x30203aa225dc2b1c892323df608368caf4846dfb459fe17cd3ddccb1f4c68449,
+            pairInitCodeHash: 0x1305842295bb388975eec146e164811bee4ac15606baa6205d5749e933f26762,
+            poolInitCodeHash: 0x574937a78cfdd44f299af1e468b13c9405677431f9e9d1eb8763760416e3bd73,
             v4PoolManager: address(0),
-            v3NFTPositionManager: 0x5161ebb7E418C593553D0dA287ad54D128aa2B9E,
+            v3NFTPositionManager: 0x16eb676BbBe51EB6E4E9DDF57BfBEe0537aA4d7B,
             v4PositionManager: address(0)
         });
 
